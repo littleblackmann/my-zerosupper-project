dba551e2c08891700a1a66982cca015f0c4dd755
littleblackmann <b0986816338@gmail.com>
2024-07-29 17:36:18 +0800

    Initial commit

--- frontend/my-zerosupper-frontend/src/main.js
+++ frontend/my-zerosupper-frontend/src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from 'vue' // 從 Vue 中導入 createApp 函數
-import App from './App.vue' // 導入根組件 App
-import router from './router' // 導入路由設定
+import { createApp } from 'vue'; // 從 Vue 中導入 createApp 函數
+import App from './App.vue'; // 導入根組件 App
+import router from './router'; // 導入路由設定
 import '@fortawesome/fontawesome-free/css/all.css';
 
-// 創建 Vue 應用實例，並使用 router 插件，然後掛載到 id 為 'app' 的 DOM 元素上
-createApp(App).use(router).mount('#app')
+// 創建 Vue 應用實例
+const app = createApp(App).use(router);
+
+// 動態引入 Google Maps API
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.VUE_APP_GOOGLE_MAPS_API_KEY}`;
+script.async = true;
+script.defer = true;
+document.head.appendChild(script);
+
+// 掛載應用
+app.mount('#app');
